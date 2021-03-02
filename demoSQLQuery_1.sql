7971d39dbcd3f488911aa572e70851a6574c196e
Chutayu Adhitayawong <73163550+24Savagez@users.noreply.github.com>
2021-03-02 22:29:12 +0700

    ...

--- demoSQLQuery_1.sql
+++
@@ -1,9 +0,0 @@
-SELECT TOP (1000) [movie_title]
-      ,[release_date]
-      ,[genre]
-      ,[mpaa_rating]
-      ,[total_gross]
-      ,[inflation_adjusted_gross]
-  FROM [disney].[dbo].[movie_gross]
-
-  select genre, sum(total_gross) total from movie_gross GROUP by genre order by total desc
